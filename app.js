7f1126daa5fd5e8538e2424436abf6de9a621fd7
DaonYoon <76722983+DaonYoon@users.noreply.github.com>
2021-11-08 16:29:43 +0900

    Javascript-basic
    chapter1 ~ object

---
+++ app.js
@@ -0,0 +1,31 @@
+// let myName = "nico";
+
+// myName = "nicolas";
+
+// console.log("your new name is " + myName);
+
+
+
+// Array(배열)
+
+
+const dayofweek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+
+// Get item from array
+console.log(dayofweek[5]);
+
+//add one more day to the array
+
+dayofweek.push("sun");
+
+// Object
+
+const playerName = "nico";
+const playerPoints = 121212;
+const playerHandsome = true;
+const playerFat = "little bit";
+
+player.name
+player.points
+player.handsome
