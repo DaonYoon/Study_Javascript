70074d7ce55332663cbc827f5fdca12bb18e0b5b
DaonYoon <76722983+DaonYoon@users.noreply.github.com>
2021-11-08 16:42:43 +0900

    Update app.js

--- app.js
+++ app.js
@@ -13,19 +13,38 @@ const dayofweek = ["mon", "tue", "wed", "thu", "fri", "sat"];
 
 
 // Get item from array
-console.log(dayofweek[5]);
+// console.log(dayofweek[5]);
 
 //add one more day to the array
 
-dayofweek.push("sun");
+// dayofweek.push("sun");
 
 // Object
 
+
+//이렇게 만드는 방식도 있다. 다만 비효율적임
 const playerName = "nico";
 const playerPoints = 121212;
-const playerHandsome = true;
+const playerHandsome = false;
 const playerFat = "little bit";
 
+
+//
+
+const player = {
+    name: "nico",
+    points: 10,
+    fat: true,
+};
+
+// player 접근방법
+console.log(player); 
+console.log(player["name"]);
+console.log(player.name);
+
+
 player.name
-player.points
-player.handsome
+player.lastName = "potato"; // 오브젝트 추가하는 방법 
+
+player.fat = false; // player 속성은 변경이 가능하다 단 전체를 바꾸는건 불가능
+console.log(player); 
