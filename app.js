4e9ae5c19e6b3983ff70a98f719ea5638ff24ebc
DaonYoon <76722983+DaonYoon@users.noreply.github.com>
2021-11-08 17:36:13 +0900

    0.0 ~ 2.9 복습!
    변수,리스트,오브젝트,함수 기능정리

--- app.js
+++ app.js
@@ -1,37 +1,66 @@
-
-// 2.7 ~ 2.8 Functions part
-
-// Functions 은 비슷한코드를 반복적으로 적지않고 더욱 적은 코드를 이용하여 
-// 더욱 효율적으로 코딩을 할수있게 해주는 것
-
-function sayHello(nameOfperson, age) { // 인자이름은 아무렇게해도 상관없긴하다.
-    console.log("Hello my name is " + nameOfperson + " my age is " + age);
-}
-
-sayHello("nico", 10); // 속성을 보내줌
-sayHello("dal", 23);
-sayHello("lynn", 21);
+// 0.0 ~ 2.8 복습!
 
 
-
-function plus(firstnumber, secondnumber) { // 더하기 함수
-    console.log(firstnumber + secondnumber);
-}
-
-plus(1, 3);
+//변수설정 
+const a = 5;  //const란 변하지않는값 상수 란 뜻
+const isDaonFat = true;
 
 
-function divide(a, b) { //나누기 함수
-    console.log(a / b);
-}
-divide(60, 8);
+isDaonFat = false; // 선언을해도 변하지않는값이기떄문에 변경이 안됨.
 
-const player = {
+
+let daonHandSome = true;
+
+
+daonhandsome = false; // let을 사용하면 변경이 가능함
+
+
+const b = null;
+
+let hello;
+console(hello); // undefined hello가 존재하지만 아무값도없다는뜻
+
+
+//create array 아무값이나 들어갈수있음
+const days = [1, 2, false, true, null, undefined,, "text"]; 
+
+const tobuy =["potato", "tomato", "pizza"];
+
+tobuy[2] // 속성의 2값을불러옴 속성은 0부터시작 ex 0..1...2
+tobuy[2] = "water"; // 속성의2값을 water로 수정함
+tobuy.push("one") // array 속성의 one 이라는 속성 추가
+
+
+// Object part
+const player = {  //player 오브젝트 생성
     name: "daon",
-    sayHello: function(otherPersonName) {
-        console.log("hello! " + otherPersonName + " Nice to meet you!");
+    age: 25,
+};
+
+player.name  // 플레이어의 name값
+console.log(player.name) // 플레이어의 name 출력
+
+player.name = "nico"; // player name값을 nico로 변경
+player.sexy = "soon"; //player 의 sexy라는 값 추가
+ 
+
+
+//function part 계산기 만들기
+
+const calcultor = {    //계산기기능 만들기
+    add:function(a, b){
+        console.log(a + b);
+    },
+    minus:function(a, b){
+        console.log(a - b);
+    },
+    div:function(a, b){
+        console.log(a / b);
+    },
+    multi:function(a, b){
+        console.log(a * b);
+    },
+    power:function(a, b){
+        console.log(a ** b);
     }
 }
-
-console.log(player.name);
-player.sayHello("lynn");
