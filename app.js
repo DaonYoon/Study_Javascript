199901092324f8d42bf3d84bb8c375907b252472
DaonYoon <76722983+DaonYoon@users.noreply.github.com>
2021-11-08 18:41:10 +0900

    2.13 ~ 2.15 Conditionals IF문
    IF 조건문과 OR, AND 학습

--- app.js
+++ app.js
@@ -1,39 +1,36 @@
-// 2.11 ~ 12 Function Returns and Recap
+// 2.13 ~ 2.15 Conditionals IF문
 
-const age = 96;
 
-function calculateKrAge(ageOfForeigner) {
-   return ageOfForeigner + 2;
+ // 이방법은 잘안씀(오래된 방식) 그이유는 코드가 이쁘지않고 css적용 불가능함
+ //const age = prompt("how old are you?");
+// console.log(typeof age); type 확인방법
+
+
+//parseInt(age);  String을 int로 변환함
+
+
+// 받는값을 숫자로 변환 숫자가아닐경우 NaN이뜸 (Not of Number)
+//const age = parseInt( prompt("How old are you?"));
+
+// console.log(age);
+
+const age = parseInt( prompt("How old are you?"));
+console.log(isNaN(age));
+
+if(isNaN(age)){ //숫자가 아닐경우 해당
+    console.log("please write a number");
 }
+else if(age < 18 ) {  //18살보다 작을떄 해당
+    console.log("your too young");
+}
+else if(age >= 18 && 30 >= age) {  //18살 이상이거나 30살 이하 둘다 충족할때 
+    console.log("You so nice age!");
+}
+else if(age < 0 || age > 100){   // 0살보다 작거나 100살보다 많으면 해당 
+    console.log("your age is ..... sorry");
+}
+//else 는 선택적사항 꼭 넣어야 하는건 아님
 
-const krAge = calculateKrAge(age);
+if((a && b) || (c && d)) {  // 뒤에꺼부터 계산됨 C , D 가 트루, a , b 가 트루, 또 둘중하나가 트루면 작동
 
-console.log(krAge);
-
-const calcultor = {    //계산기기능 만들기
-    plus:function(a, b){
-       return a + b;   //Return 할시 작업은끝남
-       console.log("hello") //이 명령문은 작동하지않음 함수는 return해버리면 function을 종료해버림
-    },
-    minus:function(a, b){
-       return a - b;
-    },
-    div:function(a, b){
-       return a / b;
-    },
-    multi:function(a, b){
-       return a * b;
-    },
-    power:function(a, b){
-       return a ** b;
-    }
-} 
-
-const plusResult = calcultor.plus(2, 3);
-const minusResult = calcultor.minus(plusResult, 10);
-const multiResult = calcultor.multi(10, minusResult);
-const divResult = calcultor.div(multiResult, plusResult);
-const powerResult = calculate.power(divResult, minusResult);
-
-
-console.log(plusResult);
+}
