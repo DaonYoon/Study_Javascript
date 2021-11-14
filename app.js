ab876fb1cbe880d94a323fc72824ed50ae87d8da
DaonYoon <76722983+DaonYoon@users.noreply.github.com>
2021-11-14 17:13:13 +0900

    ~4.7 recap

--- app.js
+++ app.js
@@ -5,27 +5,36 @@ const greeting = document.querySelector("#greeting");
 const link = document.querySelector("a");
 
 const HIDDEN_CLASSNAME = "hidden";
-
+const USERNAME_KEY = "username";
 // const loginInput = document.querySelector("#login-form input"); 쿼리셀렉터를쓸때는 이렇게
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    const username = loginInput.value;
     loginForm.classList.add("HIDDEN_CLASSNAME");
-    greeting.innerText ="hello" + username; 
-    localStorage.setItem("username", username);  // 값을 저장함
-    greeting.classList.remove("HIDDEN_CLASSNAME");
+   
+    localStorage.setItem("USERNAME_KEY", username);  // 값을 저장함
+    paingTreetings();
+   
 }
 
-
-
-
-function linkClick(event) { // 링크가 클릭됬을때 
-event.preventDefault();   // 예의 동작을 막음
-
-}
 
 loginForm.addEventListener("submit", onLoginSubmit); // 제출 클릭 이벤트
 
-link.addEventListener("click", linkClick) // 링크 클릭이벤트
+const savedUsername = localStorage.getItem("USERNAME_KEY");
 
+function paingTreetings() {
+    const username = localStorage.getItem(USERNAME_KEY);
+    greeting.innerText = "hello" + USERNAME_KEY;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+   
+}
+
+console.log(savedUsername);
+
+if(savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}
+else {
+  paingTreetings();
+}
