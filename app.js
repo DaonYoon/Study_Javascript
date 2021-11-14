0037004aa217f9aeb9e79a062e7176b6378fb490
DaonYoon <76722983+DaonYoon@users.noreply.github.com>
2021-11-14 16:41:16 +0900

    // ~4.5 saving Username

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 const loginForm = document.getElementById("login-form");
 const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
 
 const link = document.querySelector("a");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 // const loginInput = document.querySelector("#login-form input"); 쿼리셀렉터를쓸때는 이렇게
 
 function onLoginSubmit(event) {
-event.preventDefault();
-//  const username = loginInput.value;
-    console.log(loginInput.value);
+    event.preventDefault();
+    const username = loginInput.value;
+    loginForm.classList.add("HIDDEN_CLASSNAME");
+    greeting.innerText ="hello" + username; 
+    localStorage.setItem("username", username);  // 값을 저장함
+    greeting.classList.remove("HIDDEN_CLASSNAME");
 }
 
-function linkClick(event) {
-event.preventDefault();
-console.log(event);
+
+
+
+function linkClick(event) { // 링크가 클릭됬을때 
+event.preventDefault();   // 예의 동작을 막음
+
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+loginForm.addEventListener("submit", onLoginSubmit); // 제출 클릭 이벤트
 
-link.addEventListener("click", linkClick)
+link.addEventListener("click", linkClick) // 링크 클릭이벤트
 
